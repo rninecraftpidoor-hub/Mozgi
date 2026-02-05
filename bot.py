b80c42873a22f215340d4fa6c036cca445bb70ff
rninecraftpidoor-hub <rninecraftpidoor@gmail.com>
2026-02-05 09:26:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,21 +4,30 @@ import os
 
 TOKEN = os.environ["BOT_TOKEN"]
 
-async def play(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton(
-            text="🎮 Играть",
-            web_app=WebAppInfo(url="https://rninecraftpidoor-hub.github.io/Cazik/")
-        )]
-    ])
+URL = "https://rninecraftpidoor-hub.github.io/Cazik/"
 
-    await update.message.reply_text(
-        "Запусти мини-приложение 👇",
-        reply_markup=keyboard
-    )
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+await send_game(update)
+
+async def play(update: Update, context: ContextTypes.DEFAULT_TYPE):
+await send_game(update)
+
+async def send_game(update: Update):
+keyboard = InlineKeyboardMarkup([
+[InlineKeyboardButton(
+text="🎮 Играть",
+web_app=WebAppInfo(url=URL)
+)]
+])
+
+await update.message.reply_text(
+    "Запусти мини-приложение 👇",
+    reply_markup=keyboard
+)
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler(["start", "play"], play))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("play", play))
 
-app.run_polling(allowed_updates=Update.ALL_TYPES)
+app.run_polling()
